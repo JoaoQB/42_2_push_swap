95ba3cc97bcd7be04c857ebd2899f402a768f29f
jqueijo- <joaoluissobral@gmail.com>
2023-10-25 14:52:58 +0100

    next step

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:14:52 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/10/24 16:25:26 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:50:56 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int	main(int argc, char **argv)
 {
 	t_nodestack	*first;
 
+	argv++;
 	if (argc != 1)
 	{
 		first = create_stack(argv);
 		print_stack(first);
+		free(first);
 	}
 	return (0);
 }
