0fbd05fadc943b3df8dddb391e470d7aac824e9a
jqueijo- <jqueijo-@pop-os.localdomain>
2023-11-06 11:39:18 +0000

    more

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,42 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 08:19:44 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/11/06 08:19:49 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:25:18 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+static int	substr_count(char const *s, int c)
+{
+	size_t	count;
+	size_t	in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	*fill_substr(const char *src, size_t len)
+{
+	char	*dest;
+
+	if (!src)
+		return (NULL);
+	dest = (char *)malloc(sizeof(char) * len);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, src, len);
+	return (dest);
+}
