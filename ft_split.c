76ad8ab33fcee2ca0917118aa4f3c2011df4665a
jqueijo- <jqueijo-@pop-os.localdomain>
2023-11-06 12:18:40 +0000

    fixed split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 08:19:44 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/11/06 11:25:18 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/11/06 12:17:55 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,48 @@ static char	*fill_substr(const char *src, size_t len)
 		return (NULL);
 	ft_strlcpy(dest, src, len);
 	return (dest);
+}
+
+static char	**ft_free(char **strs, int count)
+{
+	while (--count <= 0)
+		free(*(strs + count));
+	free(strs);
+	return(NULL);
+}
+
+static int	iterate_i(const char *s, int i, char c)
+{
+	while (*(s + i) && *(s + i) != c)
+		i++;
+	return (i);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split_strs;
+	int		i;
+	int		j;
+	int		i_word;
+
+	split_strs = (char **)malloc((substr_count(s, c) + 1) * sizeof(char *));
+	if (!split_strs || !s)
+		return (NULL);
+	j = 0;
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			i++;
+		i_word = i;
+		i = iterate_i(s, i, c);
+		if (i_word < i)
+		{
+			*(split_strs + j++) = fill_substr(&s[i_word], (i - i_word) + 1);
+			if (!(*(split_strs + j - 1)))
+				return (ft_free(split_strs, j));
+		}
+	}
+	*(split_strs + j) = NULL;
+	return (split_strs);
 }
