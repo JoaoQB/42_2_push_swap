2337614a59165e7cde06336e35e2b39fd0657ca0
jqueijo- <jqueijo-@pop-os.localdomain>
2023-11-07 14:04:16 +0000

    push working let's gooooooooo

---
+++ push.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 10:19:11 by jqueijo-          #+#    #+#             */
+/*   Updated: 2023/11/07 13:50:45 by jqueijo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	push(t_nodestack **source, t_nodestack **dest)
+{
+	t_nodestack	*new_node;
+
+	if (!*source)
+		return ;
+	new_node = *source;
+	*source = (*source)->next;
+	if (!*dest)
+	{
+		*dest = new_node;
+		(*dest)->next = NULL;
+	}
+	else
+	{
+		(*dest)->previous = new_node;
+		new_node->next = *dest;
+		*(dest) = new_node;
+	}
+	if (*source)
+		(*source)->previous = NULL;
+}
+
+void	pa(t_nodestack **source, t_nodestack **dest)
+{
+	push(source, dest);
+	write(1, "pa\n", 3);
+	re_index(source);
+	re_index(dest);
+}
+
+void	pb(t_nodestack **source, t_nodestack **dest)
+{
+	push(source, dest);
+	write(1, "pb\n", 3);
+	re_index(source);
+	re_index(dest);
+}
+
